3d391f95925134e5199d93ba987155c35b798519
Ultradbm <mmousa367@yahoo.com>
2022-06-13 10:53:26 -0400

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace JokesWebAppMVC.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Joke Set Up")]
+        public string? JokeQuestion { get; set; }
+        [DisplayName("Punchline")]
+        public string? JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+
+     
+    }
+}
